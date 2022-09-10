ff0c52b0c9bf80c6403311c736041d642abeddeb
choiseoeun-GoUp <79019104+choiseoeun-GoUp@users.noreply.github.com>
2022-09-10 20:01:30 +0900

    add Home Pages & navigate Question Pages

--- mbti-test/src/App.js
+++ mbti-test/src/App.js
@@ -1,10 +1,17 @@
-import logo from "./logo.svg";
 import Home from "./Pages/Home";
+import Question from "./Pages/Question";
+import Result from "./Pages/Result";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
     <div>
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/question" element={<Question />} />
+        <Route path="/result" element={<Result />} />
+      </Routes>
     </div>
   );
 }
--- mbti-test/src/Pages/Home.js
+++ mbti-test/src/Pages/Home.js
@@ -1,11 +1,24 @@
 import React from "react";
 import styled from "styled-components";
+import PageImages from "../assets/ggompang.jpeg";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
+  const handleClickBtn = () => {
+    navigate("/question");
+  };
   return (
     <Wrapper>
       <Header>예비집사 판별기</Header>
-      <Title>나에게 맞는 주인님은?</Title>
+      <Contents>
+        <Title>나에게 맞는 주인님은?</Title>
+        <LogoImage>
+          <img src={PageImages} />
+        </LogoImage>
+        <Desc>MBTI를 기반으로 하는 나랑 잘 맞는 고양이 찾기</Desc>
+        <Button onClick={handleClickBtn}>테스트 시작</Button>
+      </Contents>
     </Wrapper>
   );
 };
@@ -17,12 +30,43 @@ const Wrapper = styled.div`
   height: 100vh;
   width: 100%;
 `;
-
+const Contents = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
 const Header = styled.header`
   font-size: 40px;
   display: flex;
   justify-content: center;
   align-items: center;
+  font-family: "SimKyungha";
 `;
 
-const Title = styled.div``;
+const Title = styled.div`
+  font-size: 30px;
+  margin-top: 40px;
+  font-family: "SimKyungha";
+`;
+const LogoImage = styled.div`
+  margin-top: 10px;
+
+  img {
+    width: 350px;
+    border-radius: 50%;
+  }
+`;
+const Desc = styled.div`
+  margin-top: 10px;
+  font-family: "SimKyungha";
+`;
+const Button = styled.button`
+  width: 100px;
+  height: 40px;
+  border-radius: 50px;
+  background-color: skyblue;
+  border: none;
+  margin-top: 10px;
+  font-family: "SimKyungha";
+`;
