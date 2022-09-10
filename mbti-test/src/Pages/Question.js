401d52ff81f6e5b58c745d301914eae50925864e
choiseoeun-GoUp <79019104+choiseoeun-GoUp@users.noreply.github.com>
2022-09-10 21:04:57 +0900

    add question pages

--- mbti-test/src/Pages/Question.js
+++ mbti-test/src/Pages/Question.js
@@ -1,25 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { ProgressBar, Button, ButtonGroup } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 import { QuestionData } from "../assets/data/questiondata";
 
 const Question = () => {
+  const navigate = useNavigate();
+  const [questionNo, setQuestionNo] = useState(0);
+  const [totalScore, setTotalScore] = useState([
+    { id: "EI", score: 0 },
+    { id: "SN", score: 0 },
+    { id: "TF", score: 0 },
+    { id: "JP", score: 0 },
+  ]);
+  console.log(questionNo);
+  const handleClickBtn = (no, type) => {
+    const newScore = totalScore.map((s) =>
+      s.id === type ? { id: s.id, score: s.score + no } : s
+    );
+    setTotalScore(newScore);
+
+    if (QuestionData.length !== questionNo + 1) {
+      // 다음문제로 문제수 증가
+      setQuestionNo(questionNo + 1);
+    } else {
+      // 결과 페이지 이동
+      navigate("/result");
+    }
+  };
+
   return (
     <>
       <Wrapper>
         <ProgressBar
           striped
           variant="danger"
-          now={80}
+          now={(questionNo / QuestionData.length) * 100}
           style={{ marginTop: "20px" }}
         />
-        <Title>{QuestionData[0].title}</Title>
+        <Title>{QuestionData[questionNo].title}</Title>
         <ButtonGroups>
           <Button
             style={{ width: "40%", minHeight: "200px", fontSize: "15pt" }}
+            onClick={() => handleClickBtn(1, QuestionData[questionNo].type)}
           >
-            나는 답변1번 입니다
+            {QuestionData[questionNo].answera}
           </Button>
           <Button
             style={{
@@ -28,8 +54,9 @@ const Question = () => {
               fontSize: "15pt",
               marginLeft: "20px",
             }}
+            onClick={() => handleClickBtn(0, QuestionData[questionNo].type)}
           >
-            나는 답변2번 입니다
+            {QuestionData[questionNo].answerb}
           </Button>
         </ButtonGroups>
       </Wrapper>
